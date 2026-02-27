5115d1eaa3f840e6515b47f3df555b971f694a6c
gee.chris@prophecy.io <gee.chris@prophecy.io>
2026-02-27 10:30:37 +0000

    Remove all SQL transformations and business logic from `big_query_ai_test_cg_1` pipeline

---
+++ apps/big_query_ai_test_cg_1_analysis.py
@@ -0,0 +1,5 @@
+from prophecy_analysis_sdk import *
+meta_info = MetaInfo(pipeline_id = "big_query_ai_test_cg_1", parameter_set = "")
+
+with Analysis(app_id = "big_query_ai_test_cg_1_analysis", meta_info = meta_info) as analysis:
+    pass
